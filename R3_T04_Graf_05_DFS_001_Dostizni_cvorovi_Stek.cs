395b044a2134332c26acf14487ded8b49c44296e
Dragan Ilić <147328771+draganilicnis@users.noreply.github.com>
2026-01-18 10:16:56 +0100

    Create R3_T04_Graf_05_DFS_001_Dostizni_cvorovi_Stek.cs
    Pripreme za stek, jos uvek bez steka, odnosno jos uvek rekurzivno

---
+++ R3_T04_Graf_05_DFS_001_Dostizni_cvorovi_Stek.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+class R3_T04_Graf_05_DFS_001_Dostizni_cvorovi_Stek
+{
+    static bool DFS(int Cvor_OD, int Cvor_DO, bool[] Posecen, List<int>[] Veze)
+    {
+        bool bPovezani = (Cvor_OD == Cvor_DO);  // 1. da li smo sigli smo do ciljnog rutera, ako jesmo onda je bPovezani = T i izlazimo, u suprotnom nastavljamo
+        if (!bPovezani && !Posecen[Cvor_OD])    // 2. Ako tekuci cvor (ruter) OD nije posecen (i jos uvek nije povezan sa cvorom DO)
+        {
+            Posecen[Cvor_OD] = true;                    // Oznaci da je tekuci cvor (ruter) posecen
+            int broj_susednih = Veze[Cvor_OD].Count;    // Broj susednih cvorova cvoru Cvor_OD
+            int sused_id = 0;                           // Indeks prvog susednog cvora (rutera)
+            while (sused_id < broj_susednih && !bPovezani)  // Za svaki susedni cvor (ruter) tekuceg cvora (rutera) 
+            {
+                int Cvor_Sused = Veze[Cvor_OD][sused_id];           // Trenutni susedni cvor (ruter) tekuceg cvora (rutera) Cvor_OD
+                bPovezani = DFS(Cvor_Sused, Cvor_DO, Posecen, Veze);// 3. Rekurzivni poziv DFS (susedni, DO)
+                sused_id++;                                         // Indeks sledeceg susednog cvora (rutera) cvoru Cvor_OD
+            }
+        }
+        return bPovezani;   // Vraca vrednost da li su povezani cvorovi Cvor_OD i Cvor_DO
+    }
+    static bool Korak_03_Ruteri_R12_Povezani(int Ruter_Start, int Ruter_Cilj, int broj_Rutera, List<int>[] Veze)
+    {
+        bool[] Posecen = new bool[broj_Rutera];
+        return DFS(Ruter_Start, Ruter_Cilj, Posecen, Veze);
+    }
+    static void Main()
+    {
+        List<int>[] Veze = Korak_01_Graf_Ruteri_i_Veze_Niz_Listi_Ucitaj();  // Korak 1: ULAZ: Ucitavanje Grafa
+        Korak_02_Graf_Obrada_Test_Primera(Veze);                            // Korak 2: ULAZ: Ucitavanje test primera
+    }
+    static List<int>[] Korak_01_Graf_Ruteri_i_Veze_Niz_Listi_Ucitaj()
+    {
+        int broj_Rutera = int.Parse(Console.ReadLine());
+        List<int>[] Veze = new List<int>[broj_Rutera + 1];  // + 1 -> Zato sto brojevi rutera idu od 1, a ne od 0 u test primerima
+        for (int i = 0; i < broj_Rutera + 1; i++) Veze[i] = new List<int>();
+
+        int broj_Veza = int.Parse(Console.ReadLine());
+        for (int i = 0; i < broj_Veza; i++)
+        {
+            string[] s = Console.ReadLine().Split();
+            int Ruter_OD = int.Parse(s[0]);
+            int Ruter_DO = int.Parse(s[1]);     // Ruter_OD--; Ruter_DO--;  // Zato sto brojevi rutera idu od 1, a ne od 0 u test primerima
+            Veze[Ruter_OD].Add(Ruter_DO);
+        }
+        return Veze;
+    }
+    static void Korak_02_Graf_Obrada_Test_Primera(List<int>[] Veze)
+    {
+        int broj_Rutera = Veze.Length;
+        int broj_Parova = int.Parse(Console.ReadLine());
+        for (int i = 0; i < broj_Parova; i++)
+        {
+            string[] s = Console.ReadLine().Split();
+            int Ruter_Start = int.Parse(s[0]);
+            int Ruter_Cilj = int.Parse(s[1]);   // Ruter_Start--; Ruter_Cilj--; // Zato sto brojevi rutera idu od 1, a ne od 0 u test primerima
+            bool bDa_li_su_povezani_Ruteri_R1_i_R2 = Korak_03_Ruteri_R12_Povezani(Ruter_Start, Ruter_Cilj, broj_Rutera, Veze);
+            Console.WriteLine((bDa_li_su_povezani_Ruteri_R1_i_R2) ? "da" : "ne");
+        }
+    }
+}
+
+// R3 T04 Graf 05 Obilazak: DFS: Usmereni graf:
+// https://petlja.org/sr-Latn-RS/biblioteka/r/Zbirka3/dostizni_cvorovi
+// https://app.diagrams.net/?title=R3_T04_Graf_05_Obilazak_DFS.drawio&lightbox=1&page-id=OvW9At-KTonu65IJHdt-&client=1
+// https://app.diagrams.net/#Hdraganilicnis%2FR3_T04_Graf_05_Obilazak_DFS%2Fmain%2FR3_T04_Graf_05_Obilazak_DFS.drawio#%7B%22pageId%22%3A%22OvW9At-KTonu65IJHdt-%22%7D
